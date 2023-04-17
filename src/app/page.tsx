dac081bf214a0d1ed139bae0834ae318f5e5d6bd
Igor Gomes <iguinhogms@gmail.com>
2023-04-17 01:07:15 -0300

    searchbar v1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,10 +64,16 @@ export default function Home(): any {
 
   fetchPokemon();
 
-  function redirectPage(pokemon: any): void {
+  function redirectPage(pokemon: any): any {
     router.push(`/pokemon/${pokemon.id}`);
     return pokemon.id;
   }
+
+  const filteringPokeList = (list: any): any => {
+    return list.filter((res: any) => res.name.includes(inputSearch));
+  };
+
+  const filteredPokeList = filteringPokeList(fullPokeList);
 
   return (
     <main>
@@ -81,69 +87,133 @@ export default function Home(): any {
         />
       </Header>
       <BodyContainer>
-        {fullPokeList.map((pokemon: any, i: any) => {
-          const TypeEmblem = (type: string): any => {
-            if (type === 'bug') {
-              return <BugType>Bug</BugType>;
-            } else if (type === 'dark') {
-              return <DarkType>Dark</DarkType>;
-            } else if (type === 'dragon') {
-              return <DragonType>Dragon</DragonType>;
-            } else if (type === 'electric') {
-              return <ElectricType>Electric</ElectricType>;
-            } else if (type === 'fairy') {
-              return <FairyType>Fairy</FairyType>;
-            } else if (type === 'fighting') {
-              return <FightingType>Fighting</FightingType>;
-            } else if (type === 'fire') {
-              return <FireType>Fire</FireType>;
-            } else if (type === 'flying') {
-              return <FlyingType>Flying</FlyingType>;
-            } else if (type === 'ghost') {
-              return <GhostType>Ghost</GhostType>;
-            } else if (type === 'grass') {
-              return <GrassType>Grass</GrassType>;
-            } else if (type === 'ground') {
-              return <GroundType>Ground</GroundType>;
-            } else if (type === 'ice') {
-              return <IceType>Ice</IceType>;
-            } else if (type === 'normal') {
-              return <NormalType>Normal</NormalType>;
-            } else if (type === 'poison') {
-              return <PoisonType>Poison</PoisonType>;
-            } else if (type === 'psychic') {
-              return <PsychicType>Psychic</PsychicType>;
-            } else if (type === 'rock') {
-              return <RockType>Rock</RockType>;
-            } else if (type === 'steel') {
-              return <SteelType>Steel</SteelType>;
-            } else if (type === 'water') {
-              return <WaterType>Water</WaterType>;
-            }
-          };
-          return (
-            <PokeContainer
-              key={pokemon.id}
-              onClick={() => {
-                redirectPage(pokemon);
-              }}
-            >
-              <PokeInfosContainer>
-                <PokeName>{pokemon.name.replace('-', ' ')}</PokeName>
-                <PokeTypes>
-                  {TypeEmblem(pokemon.types[0].type.name)}
-                  {pokemon.types[1]
-                    ? TypeEmblem(pokemon.types[1].type.name)
-                    : null}
-                </PokeTypes>
-              </PokeInfosContainer>
-              <PokeImageContainer>
-                <PokeBackground src={pokeBackground.src} alt="" />
-                <PokeImage src={pokemon.sprites.front_default} alt="" />
-              </PokeImageContainer>
-            </PokeContainer>
-          );
-        })}
+        {inputSearch.length > 0
+          ? filteredPokeList.map((pokemon: any, i: any) => {
+              const TypeEmblem = (type: string): any => {
+                if (type === 'bug') {
+                  return <BugType>Bug</BugType>;
+                } else if (type === 'dark') {
+                  return <DarkType>Dark</DarkType>;
+                } else if (type === 'dragon') {
+                  return <DragonType>Dragon</DragonType>;
+                } else if (type === 'electric') {
+                  return <ElectricType>Electric</ElectricType>;
+                } else if (type === 'fairy') {
+                  return <FairyType>Fairy</FairyType>;
+                } else if (type === 'fighting') {
+                  return <FightingType>Fighting</FightingType>;
+                } else if (type === 'fire') {
+                  return <FireType>Fire</FireType>;
+                } else if (type === 'flying') {
+                  return <FlyingType>Flying</FlyingType>;
+                } else if (type === 'ghost') {
+                  return <GhostType>Ghost</GhostType>;
+                } else if (type === 'grass') {
+                  return <GrassType>Grass</GrassType>;
+                } else if (type === 'ground') {
+                  return <GroundType>Ground</GroundType>;
+                } else if (type === 'ice') {
+                  return <IceType>Ice</IceType>;
+                } else if (type === 'normal') {
+                  return <NormalType>Normal</NormalType>;
+                } else if (type === 'poison') {
+                  return <PoisonType>Poison</PoisonType>;
+                } else if (type === 'psychic') {
+                  return <PsychicType>Psychic</PsychicType>;
+                } else if (type === 'rock') {
+                  return <RockType>Rock</RockType>;
+                } else if (type === 'steel') {
+                  return <SteelType>Steel</SteelType>;
+                } else if (type === 'water') {
+                  return <WaterType>Water</WaterType>;
+                }
+              };
+              return (
+                <PokeContainer
+                  key={pokemon.id}
+                  onClick={() => {
+                    redirectPage(pokemon);
+                  }}
+                >
+                  <PokeInfosContainer>
+                    <PokeName>{pokemon.name.replace('-', ' ')}</PokeName>
+                    <PokeTypes>
+                      {TypeEmblem(pokemon.types[0].type.name)}
+                      {pokemon.types[1]
+                        ? TypeEmblem(pokemon.types[1].type.name)
+                        : null}
+                    </PokeTypes>
+                  </PokeInfosContainer>
+                  <PokeImageContainer>
+                    <PokeBackground src={pokeBackground.src} alt="" />
+                    <PokeImage src={pokemon.sprites.front_default} alt="" />
+                  </PokeImageContainer>
+                </PokeContainer>
+              );
+            })
+          : fullPokeList.map((pokemon: any, i: any) => {
+              const TypeEmblem = (type: string): any => {
+                if (type === 'bug') {
+                  return <BugType>Bug</BugType>;
+                } else if (type === 'dark') {
+                  return <DarkType>Dark</DarkType>;
+                } else if (type === 'dragon') {
+                  return <DragonType>Dragon</DragonType>;
+                } else if (type === 'electric') {
+                  return <ElectricType>Electric</ElectricType>;
+                } else if (type === 'fairy') {
+                  return <FairyType>Fairy</FairyType>;
+                } else if (type === 'fighting') {
+                  return <FightingType>Fighting</FightingType>;
+                } else if (type === 'fire') {
+                  return <FireType>Fire</FireType>;
+                } else if (type === 'flying') {
+                  return <FlyingType>Flying</FlyingType>;
+                } else if (type === 'ghost') {
+                  return <GhostType>Ghost</GhostType>;
+                } else if (type === 'grass') {
+                  return <GrassType>Grass</GrassType>;
+                } else if (type === 'ground') {
+                  return <GroundType>Ground</GroundType>;
+                } else if (type === 'ice') {
+                  return <IceType>Ice</IceType>;
+                } else if (type === 'normal') {
+                  return <NormalType>Normal</NormalType>;
+                } else if (type === 'poison') {
+                  return <PoisonType>Poison</PoisonType>;
+                } else if (type === 'psychic') {
+                  return <PsychicType>Psychic</PsychicType>;
+                } else if (type === 'rock') {
+                  return <RockType>Rock</RockType>;
+                } else if (type === 'steel') {
+                  return <SteelType>Steel</SteelType>;
+                } else if (type === 'water') {
+                  return <WaterType>Water</WaterType>;
+                }
+              };
+              return (
+                <PokeContainer
+                  key={pokemon.id}
+                  onClick={() => {
+                    redirectPage(pokemon);
+                  }}
+                >
+                  <PokeInfosContainer>
+                    <PokeName>{pokemon.name.replace('-', ' ')}</PokeName>
+                    <PokeTypes>
+                      {TypeEmblem(pokemon.types[0].type.name)}
+                      {pokemon.types[1]
+                        ? TypeEmblem(pokemon.types[1].type.name)
+                        : null}
+                    </PokeTypes>
+                  </PokeInfosContainer>
+                  <PokeImageContainer>
+                    <PokeBackground src={pokeBackground.src} alt="" />
+                    <PokeImage src={pokemon.sprites.front_default} alt="" />
+                  </PokeImageContainer>
+                </PokeContainer>
+              );
+            })}
       </BodyContainer>
     </main>
   );
