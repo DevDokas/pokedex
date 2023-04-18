59b7b9c633d7cda2e3d4fd6013fef53189f7a600
Igor Gomes <iguinhogms@gmail.com>
2023-04-18 01:23:32 -0300

    page ability styling v0.2

--- src/app/ability/[id]/page.tsx
+++ src/app/ability/[id]/page.tsx
@@ -2,11 +2,25 @@
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-import { Container } from '@/components/pageAbility';
+import icon from '@/assets/icon.png';
+import {
+  Container,
+  Header,
+  Icon,
+  AbilityName,
+  AbilityID,
+  Body,
+  DescriptionContainer,
+  PokemonsContainer
+} from '@/components/pageAbility';
 
 export default function Ability(): any {
   const [abilityInfos, setAbilityInfos] = useState<any>([]);
   const [abilityDescription, setAbilityDescription] = useState<any>();
+  const [abilityPokemons, setAbilityPokemons] = useState<any>([]);
+  const [abilityPokemonsFiltered, setAbilityPokemonsFiltered] = useState<any>(
+    []
+  );
   const pathname = usePathname();
   const splitPathname = pathname.split('/');
   const abilityName = splitPathname[2];
@@ -22,19 +36,35 @@ export default function Ability(): any {
     Promise.all(abilityPromises).then((ability) => {
       const abilityInfosList = ability[0];
       setAbilityInfos(abilityInfosList);
+      setAbilityPokemons(abilityInfosList.pokemon);
       setAbilityDescription(abilityInfosList.effect_entries[0].effect);
     });
   }, [abilityName]);
 
-  console.log(abilityInfos);
+  const filtered = abilityPokemons.filter(
+    (e: any) => e.pokemon.url.split('/')[6] <= 905
+  );
 
   return (
     <Container>
-      <header>
-        {abilityName}
-        {abilityInfos.id}
-      </header>
-      {abilityDescription}
+      <Header>
+        <Icon src={icon.src} alt="" />
+        <AbilityName>{abilityName}</AbilityName>
+        <AbilityID>#{String(abilityInfos.id).padStart(3, '0')}</AbilityID>
+      </Header>
+      <Body>
+        <DescriptionContainer>{abilityDescription}</DescriptionContainer>
+        <PokemonsContainer>
+          {filtered.map((pokemons: any, i: any) => {
+            return (
+              <div key={i}>
+                {pokemons.pokemon.name.replace('-', ' ')}
+                {pokemons.pokemon.url.split('/')[6]}
+              </div>
+            );
+          })}
+        </PokemonsContainer>
+      </Body>
     </Container>
   );
 }
--- src/components/pageAbility/index.tsx
+++ src/components/pageAbility/index.tsx
@@ -1,3 +1,58 @@
+import {
+  primaryColor,
+  secondaryColor,
+  terciaryColor
+} from '@/config/colorPallet';
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Header = styled.header`
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 12vh;
+  width: 100%;
+  padding: 0 3vw 0 3vw;
+  gap: 3vw;
+  background-color: ${primaryColor};
+  z-index: 5;
+  border: 0;
+  border-radius: 0 0 1.5vh 1.5vh;
+`;
+
+export const Icon = styled.img`
+  height: 7vh;
+`;
+
+export const AbilityName = styled.h1`
+  text-transform: capitalize;
+  height: auto;
+  width: 75%;
+  padding: 1vh 3vh 1vh 3vh;
+  text-align: center;
+  background-image: linear-gradient(
+    transparent,
+    transparent,
+    rgba(0, 0, 0, 0.3)
+  );
+  border-bottom: 1px solid ${terciaryColor};
+  border-radius: 1vh;
+`;
+
+export const AbilityID = styled.h3``;
+
+export const Body = styled.section`
+  margin-top: 12.5vh;
+`;
+
+export const DescriptionContainer = styled.p`
+  padding: 2vh;
+  font-size: 2.5vh;
+  background-color: ${secondaryColor};
+  width: 90%;
+`;
+
+export const PokemonsContainer = styled.div``;
