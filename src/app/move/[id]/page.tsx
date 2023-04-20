429664be2b0141880b3e5a5eb5582a2485690c42
Igor Gomes <iguinhogms@gmail.com>
2023-04-20 01:54:56 -0300

    move page v0.2

--- src/app/move/[id]/page.tsx
+++ src/app/move/[id]/page.tsx
@@ -2,8 +2,20 @@
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
+import icon from '@/assets/icon.png';
+import {
+  Container,
+  Header,
+  Body,
+  Icon,
+  MoveName,
+  MoveID
+} from '@/components/pageMoves';
+
 export default function MovePage(): any {
   const [moveInfos, setMoveInfos] = useState<any>([]);
+  const [moveDetails, setMoveDetails] = useState<any>([]);
+  const [moveDamageClass, setMoveDamageClass] = useState<any>([]);
   const pathname = usePathname();
   const splitPathname = pathname.split('/');
   const moveName = splitPathname[2];
@@ -19,13 +31,29 @@ export default function MovePage(): any {
     Promise.all(movesPromises).then((move) => {
       const moveInfosList = move[0];
       setMoveInfos(moveInfosList);
+      setMoveDetails(moveInfosList.effect_entries[0].effect);
+      setMoveDamageClass(moveInfosList.damage_class.name);
     });
   }, [moveName]);
 
   console.log(moveInfos);
   return (
-    <div>
-      <h1>{moveName.replace('-', ' ')}</h1>
-    </div>
+    <Container>
+      <Header>
+        <Icon src={icon.src} alt="" />
+        <MoveName>{moveName.replace('-', ' ')}</MoveName>
+        <MoveID>#{String(moveInfos.id).padStart(3, '0')}</MoveID>
+      </Header>
+      <Body>
+        <h1>Efeito: {moveDetails}</h1>
+        <div>
+          <h1>Dano: {moveInfos.power}</h1>
+          <h1>Tipo de Dano: {moveDamageClass}</h1>
+          <h1>Precisao: {moveInfos.accuracy}</h1>
+          <h1>PPs: {moveInfos.pp}</h1>
+          <h1>Prioridade: {moveInfos.priority}</h1>
+        </div>
+      </Body>
+    </Container>
   );
 }
---
+++ src/components/pageMoves/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.header``;
+
+export const Body = styled.div``;
+
+export const Icon = styled.img``;
+
+export const MoveName = styled.h1`
+  text-transform: capitalize;
+`;
+
+export const MoveID = styled.h3``;
